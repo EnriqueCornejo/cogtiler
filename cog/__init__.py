dcc6f58b5b5b2b0a6207021b1d200ed2c10fe12b
Enrique Cornejo <enrique@cornejo.me>
2019-07-26 19:55:24 +0200

    updated http file

--- cog/__init__.py
+++ cog/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, abort
 from rio_tiler import main
 from rio_tiler.utils import array_to_image
 from rio_tiler.profiles import img_profiles
+from rio_tiler.errors import TileOutsideBounds
 import base64
 import io
 
@@ -12,17 +13,21 @@ app = Flask(__name__)
 @app.route('/cog/<b64url>/<z>/<x>/<y>/',  strict_slashes=False, methods=['GET'])
 def tile(b64url, x, y, z):
     url = str(base64.b64decode(b64url).decode('utf-8'))
-    app.logger.info(f"Getting tile z {z} x {x} y {y} at url {url}")
-
-    tile, mask = main.tile(
-        url,
-        int(x),
-        int(y),
-        int(z),
-        tilesize=256
-    )
-
+    app.logger.debug(f"Getting tile z {z} x {x} y {y} at url {url}")
+    try:
+        tile, mask = main.tile(
+            url,
+            int(x),
+            int(y),
+            int(z),
+            tilesize=256
+        )
+    except TileOutsideBounds as e:
+        app.logger.debug("Tile out of bounds")
+        return abort(404)
+    
     _buffer = array_to_image(tile, mask=mask, img_format="png", **image_options)
+    del tile, mask
     return send_file(
         io.BytesIO(_buffer),
         mimetype='image/png',
